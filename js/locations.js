4fef826e9762a85f723c754fde62a0e2cd843d4c
Andre Tonelli <aveigas@br.ibm.com>
2018-09-03 20:42:49 -0300

    Separation model for app

---
+++ js/locations.js
@@ -0,0 +1,33 @@
+/**
+*Data model to store locations
+*/
+var Places = [
+	{
+		name: 'Lavras Federal University',
+		address: 'UFLA - Portaria Principal - Avenida Bueno da Fonseca - Inácio Valentim, Lavras - State of Minas Gerais'
+	},
+    {
+		name: 'Jeito Caseiro Bakery',
+		address: 'Jeito Caseiro Alimentos - Praça Doutor Jorge - Dos Ipês, Lavras - State of Minas Gerais'
+	},
+    {
+		name: 'Vitória Palace Hotel',
+		address: 'Vitória Palace Hotel - Praça Doutor Augusto Silva - Centro, Lavras - State of Minas Gerais'
+	},
+    {
+		name: 'Home of Guava',
+		address: 'Rua Mante Vitorino, 510 - Aquenta Sol, Lavras - MG, 37200-000'
+	},
+    {
+		name: 'Computer Science Departament',
+		address: 'Av. Central UFLA - Centro, Lavras - MG, 37200-000'
+	},
+    {
+		name: 'Department of Food Science',
+		address: 'Universidade Federal de Lavras - Av. Doutor Sylvio Menicucci, 1001 - Centro, Lavras - MG, 37200-000'
+	},
+    {
+		name: 'Beautiful River Falls Ecological Park',
+		address: 'BR-354 - Zona Rural, Lavras - MG, 37200-000'
+	}
+];
